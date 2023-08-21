43ced3c53552d8eb812855fda357265086533715
Jeferson Rocha <111543601+jeferocha1997@users.noreply.github.com>
2023-08-21 17:25:58 -0300

    Update main.js

--- validadorCPF/main.js
+++ validadorCPF/main.js
@@ -1,40 +1,51 @@
-function validaCPF(cpfEnviado) {
+function ValidaCPF(cpfEnviado) { //  função construtora d
     Object.defineProperty(this, 'cpfLimpo', {
         enumerable: true,
         get: function () {
-            return cpfEnviado.replace(/\D+/g, ''); // Substitui o que não for número por ''
+            return cpfEnviado.replace(/\D+/g, ''); // Substitui o que não for numeral por ''
         }
     });
 }
 
-validaCPF.prototype.valida = function() {
-    if (typeof this.cpfLimpo === 'undefined')
+ValidaCPF.prototype.valida = function() {
+    if (typeof this.cpfLimpo === 'undefined' || this.cpfLimpo.length !== 11) {
         return false;
+    }
 
-    if (this.cpfLimpo.length !== 11) // Correção de 'lenght' para 'length'
+    if (this.isSequencial()) {
         return false;
+    }
 
-    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const cpfParcial = this.cpfLimpo.slice(0, -2); // Pegar os 9 primeiros dígitos
     const digito1 = this.criaDigito(cpfParcial);
-    
-    
-    return true;
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    const novoCpf = cpfParcial + digito1 + digito2;
+
+    return novoCpf === this.cpfLimpo;
 };
 
-validaCPF.prototype.criaDigito = function(cpfParcial) {
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
-
     let regressivo = cpfArray.length + 1;
-    const digito = cpfArray.reduce((ac, val) => {
-        ac += (ac * Number(val));
+
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val)); 
         regressivo--;
         return ac;
     }, 0);
-    
+
     const digito = 11 - (total % 11);
-    return digito > 9 ? 0 : digito;
+    return digito > 9 ? '0' : String(digito);
 };
 
-const cpf = new validaCPF('095.205.878-12');
-cpf.valida();
-console.log(cpf.cpfLimpo);
+ValidaCPF.prototype.isSequencial = function () { 
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCPF('095.205.878-12');
+if (cpf.valida()) {
+    console.log('CPF Válido');
+} else {
+    console.log('CPF Inválido');
+}
